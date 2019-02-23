fb004b10d2d4648b2259e0a53894224230489af1
max <glushakovmax32@gmail.com>
2019-02-23 22:53:23 +0300

    + i don't remember all changes

--- PythonNewsSentimentAnalysis/PythonNewsSentimentAnalysis/PythonNewsSentimentAnalysis - версия с сохранением в базу.py
+++ PythonNewsSentimentAnalysis/PythonNewsSentimentAnalysis/PythonNewsSentimentAnalysis - версия с сохранением в базу.py
@@ -15,30 +15,30 @@ from sentimental import Sentimental
 
 morph = pymorphy2.MorphAnalyzer()
 
-mmdasd = morph.parse('наве')
+sent = Sentimental()
+
 
 
 limitNews = 10
-minCountEntries = 3
+minCountEntries = 5
 
-tss_db = mysql.connector.connect(user='root', password='',
+tss_db = mysql.connector.connect(user='root', password='28Q{f6NA!r',
                               host='localhost',
                               database='stockthesaurus')
 
-mydbNewsParsers = mysql.connector.connect(user='root', password='',
+mydbNewsParsers = mysql.connector.connect(user='root', password='28Q{f6NA!r',
                               host='localhost',
                               database='newsparsers')
 
 
+print('Стартую')
+#try:
+sent.save_dict_to_db(sent.pathToDict[0],tss_db)
+#except Exception:
+#    print('Ошибка {}')
+print('Закончил сохранение')
+input()
 
-
-#print('Стартую')
-##try:
-#sent.save_dict_to_db(sent.pathToDict[0],tss_db)
-##except Exception:
-##    print('Ошибка {}')
-#print('Закончил сохранение')
-#input()
 def executeQuery(db, query):
     """метод выполняет запрос к базе и возвращает все данные"""
     mycursor = db.cursor()
@@ -56,6 +56,7 @@ def executeNonQuery(db, query):
 
 def oneIterSentimentAnalysis():
     """загрузка пачки новостей и их анализ"""
+    global tss_db
     lastId = 0
     maxVal = executeQuery(tss_db,"SELECT Max(idNews) FROM stockthesaurus.senty_results")[0][0]
     if maxVal is not None:
@@ -65,12 +66,11 @@ def oneIterSentimentAnalysis():
     fetchedNewsIds = executeQuery(tss_db, "call GetNewsForSentimentAnalyse({},{},{})".format(lastId,limitNews,minCountEntries))
     if len(fetchedNewsIds) > 0:
         newsIds = list(map(lambda x: int(x[0]), fetchedNewsIds))
-    else:    #вернуть для нормального функционирования
+    else:
         return None
     
     txtIds = (','.join(list(map(lambda x: str(x), newsIds))))
     newsBatch = executeQuery(mydbNewsParsers,'SELECT id, mainText FROM newsparsers.tass_parsed_news where id in ({})'.format(txtIds))
-    #newsBatch = executeQuery(mydbNewsParsers,'SELECT id, mainText FROM newsparsers.tass_parsed_news where id in (5980788)')
 
     if len(newsBatch) == 0:
         return None
@@ -79,14 +79,12 @@ def oneIterSentimentAnalysis():
     for oneNews in newsBatch:
         sentence = oneNews[1]
         result = sent.analyze(sentence)
-        print('analized one news')
         #print(result)
         dictResAnalysisNews[oneNews[0]] = result
     query = create_sql_query_insert_analysis_res(dictResAnalysisNews)
-    executeNonQuery(tss_db,query[0])
-    executeNonQuery(tss_db,query[1])
-    print('lastId {} analized and save batch {}'.format(lastId,len(newsBatch)))
-    #print(query)
+    executeNonQuery(tss_db,query)
+    print(query)
+    print(query)
     return True
 
 
@@ -97,38 +95,22 @@ def sentimentAnalysisMain():
         resultAnalysis = oneIterSentimentAnalysis()
 
 
-
 def create_sql_query_insert_analysis_res(dictResAnalysisNews:dict):
     """метод генерирует из результатов анализа sql запрос на сохранение"""
     listInsertedValues = list()
-    listInsertedFindedWords = list()
     for oneKey in dictResAnalysisNews:
         res = dictResAnalysisNews[oneKey]['allScores']
-        listInsertedValues.append('({},{},{},{},{})'.format(oneKey,res['positive'],res['negative'],res['neutral'],res['resEval']))
-        resWords = dictResAnalysisNews[oneKey]['allWords']
-        if len(resWords) > 0:
-            for oneTerm in resWords: 
-                listInsertedFindedWords.append('({},{},{},{})'.format(oneKey,oneTerm['id'],oneTerm['start'],oneTerm['stop']))
-
+        listInsertedValues.append('({},{},{},{})'.format(oneKey,res['positive'],res['negative'],res['neutral']))
 
     insertedValues = (','.join(listInsertedValues))
-    query = 'INSERT INTO senty_results (idNews, positiveValue, negativeValue, neutralValue, resEval) VALUES {} ON DUPLICATE KEY UPDATE positiveValue = VALUES(positiveValue), negativeValue = VALUES(negativeValue), neutralValue = VALUES(neutralValue), resEval = VALUES(resEval);'.format(insertedValues)
-    
-    insertedValuesFindedWords = (','.join(listInsertedFindedWords))
-    query2 = "INSERT INTO emo_finded_terms (idNews, idEmoTerm, start, stop) VALUES {};".format(insertedValuesFindedWords);
-    return (query,query2)
+    query = 'INSERT INTO senty_results (idNews, positiveValue, negativeValue, neutralValue) VALUES {} ON DUPLICATE KEY UPDATE positiveValue = VALUES(positiveValue), negativeValue = VALUES(negativeValue), neutralValue = VALUES(neutralValue);'.format(insertedValues)
+    return query
 
 
 
-resQuery = executeQuery(tss_db,'SELECT id, term, value, termLemma FROM stockthesaurus.emo_dict') #инициализация словаря
-sent = Sentimental(resQuery, None)
-
-
-timeStart = time.time()
 sentimentAnalysisMain()
-timeEnd = time.time()
-print('{} Закончили'.format(timeEnd-timeStart))
-input()
-# result = sent.analyze(sentence)
+
+
+result = sent.analyze(sentence)
     
 input()
--- PythonNewsSentimentAnalysis/PythonNewsSentimentAnalysis/sentimental.py
+++ PythonNewsSentimentAnalysis/PythonNewsSentimentAnalysis/sentimental.py
@@ -1,27 +1,83 @@
-# -*- coding: utf-8 -*-
 import re
 import os
 import csv
+import sys
 from collections import defaultdict
-
+import pymorphy2
 from razdel import tokenize
 
+morph = pymorphy2.MorphAnalyzer()
+
+
 class Sentimental(object):
-    def __init__(self, word_list=None, negation=None):
-        if word_list is None and negation is None:
-            base_dir = os.path.dirname(__file__)
-            word_list = [os.path.join(base_dir, p) for p in ['./word_list/afinn.csv', './word_list/russian.csv']]
-            negation = os.path.join(base_dir, './word_list/negations.csv')
+    def __init__(self, dataRow, negation):
+       
+        self.single_word_list = {}
+        self.multi_word_list = list()
 
-        self.word_list = {}
         self.negations = set()
+      
 
-        for wl_filename in self.__to_arg_list(word_list):
-            self.load_word_list(wl_filename)
-        for negations_filename in self.__to_arg_list(negation):
-            self.load_neagations(negations_filename)
+        res = self.convert_to_word_list(dataRow)
+        self.single_word_list = res[0]
+        self.multi_word_list = res[1]
 
-        self.__negation_skip = {'a', 'an', 'so', 'too'}
+
+
+        self.__negation_skip = {}
+     
+        
+    def convert_to_word_list(self, dataRow):
+        """Преобразует таблицу в словарь в памяти программы"""
+        singlLemmaWords = dict()
+        multiLemmaWords = list()
+
+        for oneRow in dataRow:
+            lemmatizied = oneRow[3]
+            tokenizedValues = lemmatizied.split(sep=' ')
+            if len(tokenizedValues) == 1:
+                singlLemmaWords[tokenizedValues[0]] = {'score':float(oneRow[2]), 'id':int(oneRow[0]), 'row': oneRow}
+            else:
+                multiLemmaWords.append({'lemmas': tokenizedValues,'score':float(oneRow[2]), 'id':int(oneRow[0]), 'row':  oneRow})
+        return (singlLemmaWords,multiLemmaWords)
+
+    def find_multyform_term(self,tokenizedOneNews, oneTerm):
+        """функция поиска вхождения одного терма состоящего из нескольких слов в новости"""
+        counter = int(0)
+        listTokens = list()
+        termWords = len(oneTerm['lemmas'])
+        onlyTextList = list(map(lambda x: x.text,tokenizedOneNews))
+        if oneTerm['lemmas'][0] not in onlyTextList: 
+            return (0,listTokens)
+        for iter in range(0,len(tokenizedOneNews) - termWords + 1):
+            isAllFinded = True
+            for iterTermWords in range(0,termWords):
+                if tokenizedOneNews[iter + iterTermWords].text != oneTerm['lemmas'][iterTermWords]:
+                    isAllFinded = False
+                    break
+            if isAllFinded == True:
+                listTokens.append({'id':oneTerm['id'] , 'word':' '.join(oneTerm['lemmas']),  'start':tokenizedOneNews[iter].start, 'stop': tokenizedOneNews[iter + termWords - 1].stop})
+
+                #for jiter in range(0,termWords):
+                    #listTokens.append({'id':oneTerm['id'] ,
+                    #'word':tokenizedOneNews[iter + jiter].text,
+                    #'start':tokenizedOneNews[iter + jiter].start, 'stop':
+                    #tokenizedOneNews[iter + jiter].stop})
+                    #listTokens.append(tokenizedOneNews[iter + jiter])
+                counter +=1
+
+        return (counter,listTokens)    
+    
+    def determine_range_name(self,score):
+        score_type = 'neutral'
+        if score >= 1:
+           score_type = 'positive'
+        else:
+            if score <= -1:
+                score_type = 'negative'
+            else:
+                score_type = 'neutral'
+        return score_type
 
     @staticmethod
     def __to_arg_list(obj):
@@ -35,11 +91,11 @@ class Sentimental(object):
     def __is_prefixed_by_negation(self, token_idx, tokens):
         #   True if i != 0 and tokens[i - 1] in self.negations else False
         prev_idx = token_idx - 1
-        if tokens[prev_idx] in self.__negation_skip:
+        if tokens[prev_idx].text in self.__negation_skip:
             prev_idx -= 1
 
         is_prefixed = False
-        if token_idx > 0 and prev_idx >= 0 and tokens[prev_idx] in self.negations:
+        if token_idx > 0 and prev_idx >= 0 and tokens[prev_idx].text in self.negations:
             is_prefixed = True
 
         return is_prefixed
@@ -50,61 +106,96 @@ class Sentimental(object):
             negations = set([row['token'] for row in reader])
         self.negations |= negations
 
-    def load_word_list(self, filename):
-        with open(filename, 'r') as f:
-            reader = csv.DictReader(f)
-            word_list = {row['word']: float(row['score']) for row in reader}
-        self.word_list.update(word_list)
+        
+        
+
 
     def analyze(self, sentence):
-        #sentence_clean = re.sub(r'[^\w ]', ' ', sentence.lower())
-        #tokens = sentence_clean.split()
-        tokens = tokenize(sentence)
-        scores = defaultdict(float)
-        words = defaultdict(list)
-        comparative = 0
-
-        for i, token in enumerate(tokens):
-            is_prefixed_by_negation = self.__is_prefixed_by_negation(i, tokens)
+        list_tokens = list(tokenize(sentence))
+        #tokens = list(map(lambda x: morph.normal_forms(x.text)[0],
+        #list_tokens))
+        for oneToken in list_tokens:
+            oneToken.text = morph.normal_forms(oneToken.text)[0]
+    
+        scores = defaultdict(list)
+        words = list()
+        for i, token in enumerate(list_tokens):
+            is_prefixed_by_negation = self.__is_prefixed_by_negation(i, list_tokens)
             #if token in self.word_list and not is_prefixed_by_negation:
-            if token in self.word_list:
+            if token.text in self.single_word_list:
+                findedRow = self.single_word_list[token.text]
+                score = findedRow['score'] 
+                score_type = self.determine_range_name(score)
+                               
+                scores[score_type].append(score) 
+                scores['all'].append(score) 
+                words.append({'id':findedRow['id'] , 'word':token.text,  'start':token.start, 'stop': token.stop})
+        
+        for i, term in enumerate(self.multi_word_list):
+            entries = self.find_multyform_term(list_tokens,term)
+            if entries[0] > 0:
+                score = term['score']
+                score_type = self.determine_range_name(score)
+                scores[score_type].append(score) 
+                scores['all'].append(score)
+                for oneEntry in entries[1]:
+                    words.append(oneEntry)
 
-                score = self.word_list[token]
-                if is_prefixed_by_negation:
-                    score = score *-1
 
-                score_type = 'negative' if score < 0 else 'positive'
-                scores[score_type] += score
-                words[score_type].append(token)
+        neu = 0
+        pos = 0
+        neg = 0
+        resEval = 0
 
-        if len(tokens) > 0:
-            comparative = (scores['positive'] + scores['negative']) / len(tokens)
-            scores['neutral'] = comparative
+        if len(scores['neutral']) > 0:
+            neu = average(scores['neutral'])
+
+        if len(scores['positive']) > 0:
+            pos = average(scores['positive'])
+        
+        if len(scores['negative']) > 0:
+            neg = average(scores['negative'])
+
+        if len(scores['all']) > 0:
+            resEval = average(scores['all'])
+
+        allScores = dict()
+        allScores['positive'] = pos
+        allScores['negative'] = neg
+        allScores['neutral'] = neu
+        allScores['resEval'] = resEval
 
         result = {
-            'score': scores['positive'] + scores['negative'],
-            'positive': scores['positive'],
-            'negative': scores['negative'],
-            'comparative': comparative,
-            'allScores' : scores
+            'positive': pos,
+            'negative': neg,
+            'neutral':  neu,
+            'resEval':  resEval,
+            'allScores' : allScores,
+            'allWords' : words
         }
-
         return result
+
+
+def minmax_norm(input, min, max):
+    norm = (input - min) / (max - min)
+    return norm
+
+def average(lst): 
+    return sum(lst) / len(lst) 
+
 
 
 def main():
     sent = Sentimental(word_list=['./word_list/afinn.csv', './word_list/russian.csv'],
                        negation='./word_list/negations.csv')
 
-    sentences = [
-        'Today is a very good day!',
+    sentences = ['Today is a very good day!',
         'Today is not a bad day!',
         'Today is a bad day!',
         'Сегодня хороший день!',
         'Сегодня не плохой день!',
         'Сегодня плохой день!',
-        'во весь западный демократический страна в тот число в сша запрещать реклама табачный и водочный изделие',
-    ]
+        'во весь западный демократический страна в тот число в сша запрещать реклама табачный и водочный изделие',]
 
     for s in sentences:
         result = sent.analyze(s)
